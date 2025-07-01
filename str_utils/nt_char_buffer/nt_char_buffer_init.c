b47dcf131ce81c1dfbfac8e54e0b0aa9cf5589df
ntalcme <ntalcme@gmail.com>
2025-07-01 04:42:15 +0200

    add nt_char_buffer_init function

---
+++ str_utils/nt_char_buffer/nt_char_buffer_init.c
@@ -0,0 +1,23 @@
+#include "nt_char_buffer.h"
+
+int nt_char_buffer_init(nt_char_buffer *buf, int has_arg, ...)
+{
+    va_list args;
+    size_t capacity;
+
+    capacity = NT_CHAR_BUFFER_INIT_CAPACITY;
+    if(has_arg)
+    {
+        va_start(args, has_arg);
+        capacity = va_arg(args, int);
+        va_end(args);
+    }
+
+    buf->data = malloc(sizeof(char) * capacity);
+    if (!buf->data)
+        return (0);
+    buf->len = 0;
+    buf->capacity = NT_CHAR_BUFFER_INIT_CAPACITY;
+    buf->data[0] = '\0';
+    return (1);
+}
