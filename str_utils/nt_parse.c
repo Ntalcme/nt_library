c31d31c9c19715739f8ea1f719e69d956a5a289f
ntalcme <ntalcme@gmail.com>
2025-07-02 03:37:31 +0200

    WIP parse function #7

--- str_utils/nt_parse.c
+++ str_utils/nt_parse.c
@@ -1,1 +1,11 @@
 #include "nt_str_utils.h"
+
+DEFINE_BUFFER_TYPE(char, char)
+DEFINE_BUFFER_TYPE(char *, ptr_char)
+
+char **nt_parse(const char *str)
+{
+    nt_char_buffer tmp;
+    nt_ptr_char_buffer res;
+    size_t i;
+}
