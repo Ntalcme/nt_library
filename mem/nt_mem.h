f35c2fb39ac4e13727c1ded49f446a38ba3b9946
ntalcme <ntalcme@gmail.com>
2025-07-02 19:24:27 +0200

    add nt_memmove function #12

---
+++ mem/nt_mem.h
@@ -0,0 +1,8 @@
+#ifndef NT_MEM_H
+#define NT_MEM_H
+
+# include <stddef.h>
+
+void *nt_memmove(void *dst, const void *src, size_t num_bytes);
+
+#endif
---
+++ mem/nt_memmove.c
@@ -0,0 +1,28 @@
+#include "nt_mem.h"
+
+void *nt_memmove(void *dst, const void *src, size_t num_bytes)
+{
+    unsigned char *d;
+    unsigned char *s;
+
+    if (!dst || !src) return;
+    if (dst == src) return;
+
+    d = (unsigned char *)dst;
+    s = (unsigned char *)src;
+
+    if (d > s)
+    {
+        while(num_bytes--)
+        {
+            d[num_bytes] = s[num_bytes];
+        }
+        return (dst);
+    }
+
+    while (num_bytes--)
+    {
+        *d++ = *s++;
+    }
+    return (dst);
+}
