10a3c0ebb15a80e698946757505ea6f0016ee24b
ntalcme <ntalcme@gmail.com>
2025-07-04 00:30:23 +0200

    fix

--- libnt.h
+++ libnt.h
@@ -5,6 +5,6 @@
 # include "str_utils/nt_str_utils.h"
 # include "conversion/nt_conversion.h"
 # include "nt_printf/nt_printf.h"
-# include "buffer/nt_buffer.h"
+# include "nt_buffer/nt_buffer.h"
 
 #endif
--- str_utils/nt_str_utils.h
+++ str_utils/nt_str_utils.h
@@ -6,6 +6,8 @@
 
 size_t nt_strlen(const char *str);
 char *nt_strdup(const char *str);
+nt_buffer *nt_parse(const char *str, const char sep);
+
 
 extern const char GLOBAL_NULL_CHAR;
 extern char *const GLOBAL_NULL_PTR;
