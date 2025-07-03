da328273e6ef704dedd25aacde3c33beeb4f3e99
ntalcme <ntalcme@gmail.com>
2025-07-03 04:11:43 +0200

    add nt_buffer_get doc

--- buffer/nt_buffer_get.c
+++ buffer/nt_buffer_get.c
@@ -1,5 +1,13 @@
 # include "nt_buffer.h"
 
+/**
+ * Get a pointer of nt_buffer's element at an index
+ * @param buf The buffer's pointer
+ * @param i The index
+ * @return A generic pointer (void*) to the element at the specified index
+ * Returns NULL if the buffer is invalid, empty or the index is out of bounds
+ * The returned pointed needs to be cast to the appropriate element type
+ */
 void *nt_buffer_get(nt_buffer *buf, size_t i)
 {
     if (!buf || !buf->data || i >= buf->len) return (NULL);
