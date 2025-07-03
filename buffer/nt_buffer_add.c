cec2a5f24abccc9dff582d85476efbd6922509c3
ntalcme <ntalcme@gmail.com>
2025-07-03 04:18:07 +0200

    fix typo

--- buffer/nt_buffer_add.c
+++ buffer/nt_buffer_add.c
@@ -4,7 +4,7 @@
  * Add an element to a nt_buffer struct
  * @param buf The buffer's pointer
  * @param elt The element to add
- * @return 1 if failed, 0 if success.
+ * @return 1 if failed, 0 if success
  */
 int nt_buffer_add(nt_buffer *buf, const void *elt)
 {
