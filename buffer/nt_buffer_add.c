61b47ff9c988193c285eaa70b0dad40c302b8556
ntalcme <ntalcme@gmail.com>
2025-07-03 03:57:04 +0200

    add doc nt_buffer_add

--- buffer/nt_buffer_add.c
+++ buffer/nt_buffer_add.c
@@ -1,5 +1,11 @@
 # include "nt_buffer.h"
 
+/**
+ * Add an element to a nt_buffer struct
+ * @param buf The buffer's pointer
+ * @param elt The element to add
+ * @return 1 if failed, 0 if success.
+ */
 int nt_buffer_add(nt_buffer *buf, const void *elt)
 {
     size_t new_cap;
