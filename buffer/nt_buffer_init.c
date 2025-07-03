e43753948d37ce12762e2507c09e136b2021d726
ntalcme <ntalcme@gmail.com>
2025-07-03 04:18:26 +0200

    add nt_buffer_init doc & fix bug

--- buffer/nt_buffer_init.c
+++ buffer/nt_buffer_init.c
@@ -1,5 +1,14 @@
 # include "nt_buffer.h"
 
+/**
+ * Init a buffer with a start capacity of data, 
+ * the bytes' size of elements that will be stocked in it 
+ * and a pointer of a destructor function (NULL if not needed)
+ * @param buf The buffer's pointer
+ * @param capacity The initial capacity
+ * @param element_size The bytes' size of the future stocked elements (ex: 1 for char type)
+ * @return 1 if failed, 0 if success
+ */
 int nt_buffer_init(nt_buffer *buf, size_t capacity, size_t element_size, void (*destructor)(void*))
 {
     if (!buf || element_size == 0) return (1);
@@ -8,7 +17,7 @@ int nt_buffer_init(nt_buffer *buf, size_t capacity, size_t element_size, void (*
     if (capacity > 0)
     {
         buf->data = malloc(element_size * capacity);
-        if (!buf) return (1);
+        if (!buf->data) return (1);
     }
     buf->capacity = capacity;
     buf->len = 0;
