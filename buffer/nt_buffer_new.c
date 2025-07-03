bdc3fe00bf33cd92fafb90cbd68e10388139e0af
ntalcme <ntalcme@gmail.com>
2025-07-03 04:23:25 +0200

    add nt_buffer_new doc

--- buffer/nt_buffer_new.c
+++ buffer/nt_buffer_new.c
@@ -1,6 +1,12 @@
 #include "nt_buffer.h"
 
-
+/**
+ * Allocate and init a nt_buffer struct with specified params
+ * @param capacity The initial capacity
+ * @param element_size The bytes' size of the future stocked elements (ex: 1 for char type)
+ * @param destructor An appropriate destructor function pointer (NULL if not needed)
+ * @return The new nt_buffer's pointer (NULL if any error)
+ */
 nt_buffer *nt_buffer_new(size_t capacity, size_t element_size, void (*destructor)(void*))
 {
     nt_buffer *res;
