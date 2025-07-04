3a391c7c77030b8f094397f941d42eba81558c42
ntalcme <ntalcme@gmail.com>
2025-07-04 19:07:43 +0200

    fix

--- nt_printf/nt_printf.h
+++ nt_printf/nt_printf.h
@@ -2,7 +2,9 @@
 #define NT_PRINTF_H
 
 # include <stdarg.h>
-# include "../libnt.h"
+# include "../conversion/nt_conversion.h"
+# include "../str_utils/nt_str_utils.h"
+# include "../fd/nt_fd.h"
 
 ssize_t nt_printf(const char *str, ...);
 
