9976e893d60309b528552e8ebe328fa9f3008382
ntalcme <ntalcme@gmail.com>
2025-07-04 04:51:04 +0200

    add nt_join function

---
+++ str_utils/nt_join.c
@@ -0,0 +1,67 @@
+# include "nt_str_utils.h"
+# include <stdio.h>
+
+static int add_string(nt_buffer *buf, char *str)
+{
+    size_t new_len;
+    size_t new_cap;
+    char *new_data;
+    size_t str_len;
+
+    if (!buf || !str) return (1);
+    if (buf->element_size != sizeof(char)) return (1);
+
+    str_len = nt_strlen(str);
+    new_len = buf->len + str_len;
+    
+    if (new_len >= buf->capacity)
+    {
+        new_cap = buf->capacity ? buf->capacity * 2 : 4;
+        while (new_len > new_cap)
+        {
+            new_cap *= 2;
+        }
+        new_data = realloc(buf->data, new_cap * buf->element_size);
+        if (!new_data) return (1);
+
+        buf->data = new_data;
+        buf->capacity = new_cap;
+    }
+
+
+    nt_memmove((char *)buf->data + buf->len, str, str_len);
+
+    buf->len = new_len;
+    return (0);
+}
+
+char *nt_join(nt_buffer *buf, const char sep)
+{
+    nt_buffer str;
+    char *tmp;
+    size_t i;
+    char *res;
+
+    if (!buf || !buf->data || buf->len == 0) return (NULL);
+    if (nt_buffer_init(&str, 32, sizeof(char), NULL)) return (NULL);
+
+    for (i = 0; i < buf->len - 1; i++)
+    {
+        tmp = *(char **)nt_buffer_get(buf, i);
+        if (!tmp) return (NULL);
+
+        if (add_string(&str, tmp)) return (NULL);
+        if (i < buf->len - 2) if (nt_buffer_add(&str, &sep)) return (NULL);
+    }
+
+    if (nt_buffer_add(&str, &GLOBAL_NULL_CHAR))
+    {
+            nt_buffer_free(&str);
+            return (NULL);
+    }
+
+    res = nt_strdup(str.data);
+    nt_buffer_free(&str);
+    if (!res) return (NULL);
+    return (res);
+}
--- str_utils/nt_str_utils.h
+++ str_utils/nt_str_utils.h
@@ -7,7 +7,7 @@
 size_t nt_strlen(const char *str);
 char *nt_strdup(const char *str);
 nt_buffer *nt_parse(const char *str, const char sep);
-
+char *nt_join(nt_buffer *buf, const char sep);
 
 extern const char GLOBAL_NULL_CHAR;
 extern char *const GLOBAL_NULL_PTR;
